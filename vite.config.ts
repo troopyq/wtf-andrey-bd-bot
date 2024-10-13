0223101be3ec477b09f21c39f28e927dede75f9c
Немков Дмитрий <nemkov03@mail.ru>
2024-10-13 05:00:42 +0500

    add dynamic import

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dynamicImportVars from 'vite-plugin-dynamic-import';
 // import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-
+  plugins: [react(), dynamicImportVars()],
   build: {
     outDir: './docs',
   },
